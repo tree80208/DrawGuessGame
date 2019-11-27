035e4ea9b5446d39691a6e2cd7a3a063e475188e
evenegas3 <evenegas3@users.noreply.github.com>
2019-11-27 15:51:21 -0700

    Update drawing frag
    Still needs word and timer divs on fragment

--- app/src/main/java/com/example/drawguessgame/MainActivity.java
+++ app/src/main/java/com/example/drawguessgame/MainActivity.java
@@ -1,219 +1,208 @@
-package com.example.drawguessgame;
+package com.example.eujinsketch;
 
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
+import android.graphics.Canvas;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.util.Log;
+import android.os.Environment;
+import android.provider.ContactsContract;
 import android.view.Gravity;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserProfileChangeRequest;
-
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    FirebaseAuth mAuth;
-    FirebaseUser currentUser;
-    RegisterFragment frag;
-    Uri imageUri = null;
-    private static final String TAG = "EmailPassword";
-    final int RESULT_LOAD_IMG = 1;
 
-
+    public static DrawingFragment drawingFragment = null;
+    // Color available, default = red
+    int[] color_arr = {R.color.colorRed, R.color.colorGreen, R.color.colorBlue,
+            R.color.colorPurple, R.color.aqua, R.color.pink, R.color.grey, R.color.orange,
+            R.color.colorWhite};
+    float[] size_arr = {15,30,60};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mAuth =  FirebaseAuth.getInstance();
-    }
+        this.drawingFragment = new DrawingFragment();
+        drawingFragment.setContainerActivity(this);
 
-    @Override
-    public void onStart(){
-        super.onStart();
-        currentUser = mAuth.getCurrentUser();
-        //TODO: Launch screen 2
-        System.out.println("OnStart: "+currentUser.toString());
-    }
+        FragmentTransaction transaction = getSupportFragmentManager()
+                .beginTransaction();
+        transaction.add(R.id.draw_layout,drawingFragment);
 
-    public void buttonLogin(View v){
-        String email = ((EditText)findViewById(R.id.screen1_edit_text_id)).getText().toString();
-        String password = ((EditText)findViewById(R.id.screen1_edit_text_pwd)).getText().toString();
-
-        if(email.trim().equals("") || password.trim().equals("")){
-            showToast("Please Enter User ID/PASSWORD",Toast.LENGTH_SHORT);
-            return;
-        }
-
-        mAuth.signInWithEmailAndPassword(email,password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            Log.d(TAG,"signInWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            showToast("Login Successful", Toast.LENGTH_SHORT);
-                            //TODO: Launch screen 2
-                            launchChooseGameActivity();
-
-
-                        }else{
-                            Log.d(TAG,"signInWithEmail:failure",task.getException());
-                            showToast("Login Unsuccessful", Toast.LENGTH_SHORT);
-                        }
-                    }
-                });
-    }
-
-    //TODO: NEED to send Authentication information
-    public void launchChooseGameActivity(){
-        Intent intent = new Intent(this, ChooseGameActivity.class);
-        startActivity(intent);
-    }
-
-    public void showToast(String str, int attr){
-        Toast toast = Toast.makeText(MainActivity.this,str,attr);
-        toast.setGravity(Gravity.CENTER,0,0);
-        toast.show();
-    }
-
-    public void registerAccount(View v){
-        frag = new RegisterFragment();
-        frag.setContainerActivity(this);
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.add(R.id.screen1_frame,frag,"REGISTER");
         transaction.addToBackStack(null);
         transaction.commit();
     }
-
-    public void signUpNewAccount(View view) {
-        String email = ((EditText)findViewById(R.id.screen1_register_edit_text_id)).getText().toString();
-        String password = ((EditText)findViewById(R.id.screen1_register_edit_text_pwd)).getText().toString();
-
-        final String name = ((EditText)findViewById(R.id.screen1_register_edit_text_name)).getText().toString();
-
-        if(email.trim().equals("") || password.trim().equals("") || name.trim().equals("")){
-            showToast("Please fill out all information", Toast.LENGTH_SHORT);
-            return;
-        }
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success");
-                            currentUser = mAuth.getCurrentUser();
-                            setProfile(name);
-                            showToast("Sign up successful", Toast.LENGTH_SHORT);
-                            exitFragment();
-
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            showToast("Register failed.\nPassword should be at least 6 characters.", Toast.LENGTH_LONG);
-                        }
-                    }
-                });
+    public void clearDrawing(View v){
+        DrawingView dv = drawingFragment.getDrawingView();
+        dv.startNew();
     }
 
-    public void setProfile(String name){
+    public void changeBrushSize(View v){
+        DrawingView dv = drawingFragment.getDrawingView();
 
-        if(imageUri == null){
-            imageUri = Uri.parse("android.resource://com.example.drawguessgame/drawable/cat.jpg");
+        int viewID = v.getId();
+        if(viewID == findViewById(R.id.button_small).getId()){
+            dv.changePaintSize(size_arr[0]);
+        }else if(viewID == findViewById(R.id.button_medium).getId()){
+            dv.changePaintSize(size_arr[1]);
+        }else if(viewID == findViewById(R.id.button_large).getId()){
+            dv.changePaintSize(size_arr[2]);
         }
 
-        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
-                .setDisplayName(name)
-                .setPhotoUri(imageUri)
-                .build();
-
-        currentUser.updateProfile(profileUpdates)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if(task.isSuccessful()){
-                            Log.d(TAG, "User profile updated");
-                        }
-                    }
-                });
+//        else if(viewID == findViewById(R.id.button_erase).getId()){
+////            dv.changePaintColor(color_arr[8]);
+////
+////        }
     }
 
     /**
-     * Launches Gallery to fetch Image
+     * This function calls the contacts list fragment
+     *  which shows lists of contact and lets the user select
+     *  which person to share with
+     * @param v
      */
-    public void getImageFromGallery(View v){
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        intent.setType("image/*");
-        startActivityForResult(intent, RESULT_LOAD_IMG);
+//    public void onClickShare(View v){
+//        ContactsFragment frag = new ContactsFragment();
+//        frag.setContainerActivity(this);
+//        FragmentTransaction transaction = getSupportFragmentManager()
+//                .beginTransaction();
+//
+//        transaction.add(R.id.screen_layout,frag);
+//
+//        transaction.addToBackStack(null);
+//        transaction.commit();
+//    }
+
+    /**
+     * This function changes the paint brush color
+     * @param v
+     */
+    public void changePaintColor(View v){
+        DrawingView dv = drawingFragment.getDrawingView();
+
+        int viewID = v.getId();
+        if(viewID == findViewById(R.id.button_red).getId()){
+            dv.changePaintColor(color_arr[0]);
+        }else if(viewID == findViewById(R.id.button_green).getId()){
+            dv.changePaintColor(color_arr[1]);
+        }else if(viewID == findViewById(R.id.button_blue).getId()){
+            dv.changePaintColor(color_arr[2]);
+        }else if(viewID == findViewById(R.id.button_purple).getId()){
+            dv.changePaintColor(color_arr[3]);
+        }else if(viewID == findViewById(R.id.button_aqua).getId()){
+            dv.changePaintColor(color_arr[4]);
+        }else if(viewID == findViewById(R.id.button_pink).getId()){
+            dv.changePaintColor(color_arr[5]);
+        }else if(viewID == findViewById(R.id.button_grey).getId()){
+            dv.changePaintColor(color_arr[6]);
+        }else if(viewID == findViewById(R.id.button_orange).getId()){
+            dv.changePaintColor(color_arr[7]);
+        }else if(viewID == findViewById(R.id.button_erase).getId()){
+            dv.changePaintColor(color_arr[8]);
+        }
+
+
+
+
     }
 
     /**
-     * Executed when returning from Gallery Activity
-     * @param requestCode
-     * @param resultCode
-     * @param data
+     * This function clicks the contact and launches mail app
+     *  to share the bitmap image
+     * @param v
      */
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode,resultCode,data);
+    public void clickContact(View v){
+        ListView listView = (ListView) v.getParent();
+        int contactId = listView.getPositionForView(v)+1;
 
-        if(resultCode == RESULT_OK){
-            try{
-                imageUri = data.getData();
-                InputStream imageStream = getContentResolver().openInputStream(imageUri);
-                Bitmap image = BitmapFactory.decodeStream(imageStream);
-                //TODO: RESIZE IMAGE
-                ImageView imageView = (ImageView)findViewById(R.id.screen1_register_profile_img);
-                imageView.setImageBitmap(image);
-            } catch (FileNotFoundException e) {
-                showToast("File Not Found",Toast.LENGTH_SHORT);
+        System.out.println("CONTACT_ID: "+contactId);
+
+        Cursor emails = getContentResolver().query(
+                ContactsContract.CommonDataKinds.Email.CONTENT_URI, null,
+                ContactsContract.CommonDataKinds.Email.CONTACT_ID
+                        + " = " + contactId, null, null);
+
+        List<String> emailsList = new ArrayList<>();
+        if(emails.moveToNext()) {
+            String email = emails
+                    .getString(emails.getColumnIndex(
+                            ContactsContract.CommonDataKinds.Email.ADDRESS));
+            System.out.println(email);
+            if(!email.trim().equals("")){
+                emailsList.add(email);
+                launchMail(email);
             }
-        }else{
-            showToast("Image not picked",Toast.LENGTH_SHORT);
         }
+        emails.close();
+
+//        launchMail(emailsList);
     }
 
     /**
-     * Exits the register fragment if the sign up was successful
+     * This function calls intent with email address
+     * @param address
      */
-    public void exitFragment(){
-        imageUri = null;
-        String id = ((EditText)findViewById(R.id.screen1_register_edit_text_id)).getText().toString();
-        String pwd = ((EditText)findViewById(R.id.screen1_register_edit_text_pwd)).getText().toString();
+    public void launchMail(String address){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("vnd.android.cursor.dir/email");
 
-        EditText id_placeholder = (EditText) findViewById(R.id.screen1_edit_text_id);
-        EditText pwd_placeholder = (EditText) findViewById(R.id.screen1_edit_text_pwd);
+        intent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[] { address });
+        System.out.println(BuildConfig.APPLICATION_ID +".provider");
 
-        id_placeholder.setText(id);
-        pwd_placeholder.setText(pwd);
+        Uri uri = saveImageToFile(drawingFragment.getDrawingView().getBitmap());
+        intent.putExtra(android.content.Intent.EXTRA_STREAM, uri);
 
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        startActivity(intent);
+    }
 
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.remove(frag);
-        transaction.commit();
+    public Uri saveImageToFile(Bitmap bitmap){
+        //creates bitmap to draw on
+        Canvas canvas = new Canvas(bitmap);
+
+        Uri ImageUri=null;
+        try{
+            File imageFile = createImageFile();
+            FileOutputStream out = new FileOutputStream(imageFile);
+            bitmap.compress(Bitmap.CompressFormat.PNG,100,out);
+
+            //Saves the uri for the view group image file
+            ImageUri = FileProvider.getUriForFile(
+                    this,"com.example.Sketcher.FileProvider",
+                    imageFile);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return ImageUri;
+    }
+
+    // This function creates empty file directory to save image
+    private File createImageFile() throws Exception{
+        String timeStamp = new SimpleDateFormat(
+                "yyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_"+timeStamp+"_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName,".jpg",storageDir);
+
+        return image;
     }
 }
